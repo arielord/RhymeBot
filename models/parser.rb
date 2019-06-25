7ea9205fce6af3fa3ead0b143ab82db66ea7d0e3
arielord <arielord@buffalo.edu>
2019-06-25 13:49:49 -0400

    parsing method.

--- models/parser.rb
+++ models/parser.rb
@@ -1,3 +1,20 @@
 class Parser
+  @@all = {}
 
+  def self.pronunciationParser()
+    cmudict = "../pronunciationDictionary/cmudict-0.7b.txt"
+    File.foreach(cmudict) do |line|
+      if line[0] < 'A'
+        next
+      elsif line[0] > 'Z'
+        next
+      else
+        @@all[line.split()[0]] = line.split()[1...line.split.length()]
+      end
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
