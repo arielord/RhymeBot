6cce8a107818806e8a6ac00ebe1dc4bd7a13f557
arielord <arielord@buffalo.edu>
2019-07-02 00:59:29 -0400

    iterating over index instead to access both syllables from both words.

--- models/rhymebot.rb
+++ models/rhymebot.rb
@@ -8,7 +8,6 @@ class Rhymebot
 
   def self.find_best_rhymes(word)
     best_rhymes = {}
-    counter = 0
     word = Word.find(word.upcase)
 
     rs = word.rhyming_syllable
@@ -16,7 +15,24 @@ class Rhymebot
     syll = word.syllables
 
     @@rhymes[rs].each do |rhyme|
+      counter = 0
 
+      rhyme_rs = rhyme.rhyming_syllable
+      rhyme_word = rhyme.word
+      rhyme_syll = rhyme.syllables
+
+      if syll.length <= rhyme_syll
+        less_syll = syll
+        more_syll = rhyme_syll
+      else
+        less_syll = rhyme_syll
+        more_syll = syll
+      end
+
+      for i in 1..less_syll.length
+        if less_syll[-i] == more_syll[-i]
+          counter = counter + 1
+      end
     end
   end
 
