6e07c0aeda3bd7b58fb641f096d14da5bbcd6394
arielord <arielord@buffalo.edu>
2019-07-01 20:55:30 -0400

    Setting up for loop for find best rhyme.

--- models/rhymebot.rb
+++ models/rhymebot.rb
@@ -1,4 +1,5 @@
 class Rhymebot
+
   @@rhymes = {}
 
   def initialize()
@@ -6,10 +7,17 @@ class Rhymebot
   end
 
   def self.find_best_rhymes(word)
+    best_rhymes = {}
     counter = 0
     word = Word.find(word.upcase)
 
-    
+    rs = word.rhyming_syllable
+    w = word.word
+    syll = word.syllables
+
+    @@rhymes[rs].each do |rhyme|
+
+    end
   end
 
   def self.rhymes
