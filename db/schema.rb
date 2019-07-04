14631a5912e85a3a8967cd3525f1fdb5a8bf42a9
arielord <arielord@buffalo.edu>
2019-07-04 03:52:52 -0400

    setting up rake and activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_04_074222) do
+
+  create_table "words", force: :cascade do |t|
+    t.string "word"
+    t.string "syllables"
+  end
 
 end
